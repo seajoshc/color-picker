1dd7515b1e9c6cdaa1d7635a866057adc1ccb676
Josh Campbell <josh@p4a.net>
2023-06-16 20:28:56 -0700

    adds colorpicker

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "color_picker.apps.ColorPickerConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
