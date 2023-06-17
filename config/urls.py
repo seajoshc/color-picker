52e320ab3002e07ab52831815c3b54553c5bc994
Josh Campbell <josh@p4a.net>
2023-06-16 20:46:18 -0700

    sets color_picker app to root, formatting

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("color_picker/", include("color_picker.urls")),
-    path('admin/', admin.site.urls),
+    path("", include("color_picker.urls")),
+    path("admin/", admin.site.urls),
 ]
